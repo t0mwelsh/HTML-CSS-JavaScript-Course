e65bc85bb2ebed67c0a8b06c6f180030057afd71
t0mwelsh <tomas.welsh@btinternet.com>
2023-09-14 12:17:33 +0100

    solution for module 4 assignment

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,14 @@
+//SpeakGoodBye.js
+(function (window) {
+
+var byeSpeaker = {};
+
+byeSpeaker.speakWord = "Good Bye";
+
+byeSpeaker.speak = function (name) {
+  console.log(byeSpeaker.speakWord + " " + name);
+}
+
+window.byeSpeaker = byeSpeaker;
+
+})(window);
